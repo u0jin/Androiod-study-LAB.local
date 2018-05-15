d2c2b36c085c6751724be1546b0af3827ddf6ec2
youjin kim <38131695+u0jin@users.noreply.github.com>
2018-05-15 17:15:36 +0900

    화면 전환 수정

--- manageApp/app/src/main/java/com/example/ujin/manageapp/MainActivity.java
+++ manageApp/app/src/main/java/com/example/ujin/manageapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
     public void viewThingsBtn(View v){
         Intent myIntent=new Intent(getApplicationContext(),viewThings.class);
         startActivity(myIntent);
+        finish();
 
     }
     public void searchThingsBtn(View v){
-        Intent myIntent=new Intent(getApplicationContext(),viewThings.class);
+        Intent myIntent=new Intent(getApplicationContext(),searchThings.class);
         startActivity(myIntent);
+        finish();
     }
     public void enrollThingsBtn(View v){
-        Intent myIntent=new Intent(getApplicationContext(),viewThings.class);
+        Intent myIntent=new Intent(getApplicationContext(),enrollThings.class);
         startActivity(myIntent);
+        finish();
     }
 }
--- manageApp/app/src/main/java/com/example/ujin/manageapp/searchThings.java
+++ manageApp/app/src/main/java/com/example/ujin/manageapp/searchThings.java
@@ -4,25 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
-public class viewThings extends AppCompatActivity {
+public class searchThings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewthings);
+        setContentView(R.layout.searchthings);
 
 
     }
     public void backBtn(View v) {
         Intent myIntent=new Intent(getApplicationContext(),MainActivity.class);
         startActivity(myIntent);
-    }
 
-    public void dailySuppliesBtn(View v){
-        Intent myIntent=new Intent(getApplicationContext(),detailthings.class);
-        startActivity(myIntent);
+
     }
 
 
